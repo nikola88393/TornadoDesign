a3aed88b21b30008672c8ed7a58056e8618eba5d
Nikola <nikola88393@gmail.com>
2024-10-11 15:02:03 +0300

    Create responsive header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
-import { version } from "antd";
+import { version, Layout, ConfigProvider } from "antd";
 import AppHeader from "./components/AppHeader";
-import { Layout } from "antd";
+import {} from "antd";
 const { Header, Footer, Content } = Layout;
 
 const App = () => {
   return (
-    <Layout>
-      <Header style={{ display: "flex" }}>
-        <AppHeader />
-      </Header>
-      <Content style={{ padding: "0 50px" }}>
-        <div style={{ background: "#fff", padding: 24, minHeight: 280 }}>
-          <h1>Welcome to Ant Design {version}</h1>
-        </div>
-      </Content>
-      <Footer style={{ textAlign: "center" }}>
-        Ant Design ©2018 Created by Ant UED
-      </Footer>
-    </Layout>
+    <ConfigProvider
+      theme={{
+        token: {
+          Layout: {
+            headerBg: "#ffffff",
+            headerColor: "#ffffff",
+          },
+          // Seed Token
+        },
+      }}
+    >
+      <Layout>
+        <Header style={{ display: "flex" }}>
+          <AppHeader />
+        </Header>
+        <Content style={{ padding: "0 50px" }}>
+          <div style={{ background: "#fff", padding: 24, minHeight: 280 }}>
+            <h1>Welcome to Ant Design {version}</h1>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
+    </ConfigProvider>
   );
 };
 
--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,24 +1,60 @@
-import { Menu } from "antd";
+import { Menu, Button, Drawer } from "antd";
+import { MenuOutlined } from "@ant-design/icons";
 import logo from "../assets/tornado_logo.png"; // optional for custom styling
-
+import logoText from "../assets/tornado_logo_text.png"; // optional for custom styling
+import { useState } from "react";
+import "../styles/AppHeader.css";
 const AppHeader = () => {
+  const [visible, setVisible] = useState(false);
+
+  const showDrawer = () => {
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
   return (
     <>
       <div className="logo">
-        <img src={logo} alt="logo" width={67} />
+        <img src={logo} alt="logo" height={50} />
+        <img src={logoText} alt="logo" height={30} />
       </div>
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        defaultSelectedKeys={["1"]}
-        style={{ width: "100%", display: "flex", justifyContent: "right" }}
-      >
-        <Menu.Item key="1">Начало</Menu.Item>
-        <Menu.Item key="2">Услуги</Menu.Item>
-        <Menu.Item key="3">Цени</Menu.Item>
-        <Menu.Item key="4">За нас</Menu.Item>
-        <Menu.Item key="5">Контакти</Menu.Item>
-      </Menu>
+      <div className="menu-desktop-container">
+        <Menu
+          className="menu-desktop"
+          theme="light"
+          mode="horizontal"
+          defaultSelectedKeys={["1"]}
+        >
+          <Menu.Item key="1">Начало</Menu.Item>
+          <Menu.Item key="2">Услуги</Menu.Item>
+          <Menu.Item key="3">Цени</Menu.Item>
+          <Menu.Item key="4">За нас</Menu.Item>
+          <Menu.Item key="5">Контакти</Menu.Item>
+        </Menu>
+      </div>
+      <div className="menu-mobile-container">
+        <Button
+          variant="outlined"
+          size="large"
+          icon={<MenuOutlined />}
+          onClick={showDrawer}
+        ></Button>
+        <Drawer title="Menu" placement="right" onClose={onClose} open={visible}>
+          <Menu
+            className="menu-mobile"
+            mode="vertical"
+            defaultSelectedKeys={["1"]}
+          >
+            <Menu.Item key="1">Начало</Menu.Item>
+            <Menu.Item key="2">Услуги</Menu.Item>
+            <Menu.Item key="3">Цени</Menu.Item>
+            <Menu.Item key="4">За нас</Menu.Item>
+            <Menu.Item key="5">Контакти</Menu.Item>
+          </Menu>
+        </Drawer>
+      </div>
     </>
   );
 };
