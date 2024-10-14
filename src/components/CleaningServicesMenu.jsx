731f94f9a663b2e7d02925dbfaa03396b103fd37
Nikola <nikola88393@gmail.com>
2024-10-15 00:12:04 +0300

    Integrate lottie animations for all services

--- src/components/CleaningServicesMenu.jsx
+++ src/components/CleaningServicesMenu.jsx
@@ -1,69 +1,98 @@
-import { Card, Modal, Button } from "antd";
-import { HomeOutlined, CarOutlined, ShopOutlined } from "@ant-design/icons";
+import { Card, Modal, Button, Flex } from "antd";
 import { useState } from "react";
 
+const DotLottieIcon = ({ src }) => {
+  return (
+    <dotlottie-player
+      src={src}
+      background="transparent"
+      speed="1"
+      style={{ width: "150px", height: "150px" }}
+      loop
+      autoplay
+    ></dotlottie-player>
+  );
+};
 const services = [
   {
     title: "Основно почистване",
     description:
       "Почистване на трудно достъпни места. Обезпрашаване, почистване на дограма и прозорци вътрешно (външно ако са достъпни). Основно почистване на кухня, хладилник (ако е празен), фурна, котлони, шкафове външно (вътрешно ако са празни). Почистване на баня - душ, вана, душкабина, тоалетна, фуги и премахване на котлен камък.",
-    icon: HomeOutlined,
+    icon: (
+      <DotLottieIcon src="https://lottie.host/eef38f27-d573-4b97-ae10-ab37affaa871/dIVoNxHfer.json" />
+    ),
     key: "basic_cleaning",
   },
   {
     title: "Почистване след ремонт",
     description:
       "Услугата включва премахване на следи от ремонтни дейности, обезпрашаване, почистване на дограми и прозорци, почистване на баня, тоалетна и кухня.",
-    icon: HomeOutlined,
+    icon: (
+      <DotLottieIcon src="https://lottie.host/c04834fe-0081-45e3-b5fa-b0b99ce04517/afRr9JUimR.json" />
+    ),
     key: "post_renovation_cleaning",
   },
   {
     title: "Пране на мека мебел и килими на място",
     description:
       "Услугата включва машинно пране на мека мебел и килими с безвредни и качествени препарати. След изпиране са нужни между 3 и 8 часа за пълно изсъхване според зависимост от материята и това колко вода поема.",
-    icon: CarOutlined,
+    icon: (
+      <DotLottieIcon src="https://lottie.host/9d7b5157-cb43-4333-958c-acbe4967fda0/ROva1Oxomf.json" />
+    ),
     key: "upholstery_carpet_cleaning",
   },
   {
     title: "Експресно почистване",
     description:
       "Услугата включва 2 часа обезпрашаване, прахосмукиране, почистване на баня, кухня и мопиране на твърди подови настилки.",
-    icon: HomeOutlined,
+    icon: (
+      <DotLottieIcon src="https://lottie.host/22eb5309-4e27-47f4-b612-7b3720f95938/bwKxPA3nUB.json" />
+    ),
     key: "express_cleaning",
   },
   {
     title: "Пране на автотапицерия",
     description:
       "Услугата включва пране на седалки, подове и багажник. Освежаване на таблото и подлакътник. Тавани и кори не се перат. Услугата се извършва на ваш адрес или в гаража на Торнадо Сървисиз.",
-    icon: CarOutlined,
+    icon: (
+      <DotLottieIcon src="https://assets5.lottiefiles.com/packages/lf20_tdllc410.json" />
+    ),
     key: "car_upholstery_cleaning",
   },
   {
-    title: "Полиране на фарове и поставяне на защитно фолио",
+    title: "Грижа за фаровете",
     description:
       "Услугата се предлага на ваш адрес или в гаража на Торнадо Сървисиз.",
-    icon: CarOutlined,
+    icon: (
+      <DotLottieIcon src="https://lottie.host/d29a80c9-694b-4d07-bd52-c8e3b8c2cfd1/KQWFDAVfXz.json" />
+    ),
     key: "headlight_polishing",
   },
   {
-    title: "Хамалски услуги, Изхвърляне на боклук, и пренасяне на багаж",
+    title: "Хамалски услуги",
     description:
       "Услугата включва хамалски услуги, изхвърляне на боклук и пренасяне на багаж.",
-    icon: ShopOutlined,
+    icon: (
+      <DotLottieIcon src="https://assets3.lottiefiles.com/packages/lf20_7foh1e6l.json" />
+    ),
     key: "moving_services",
   },
   {
     title: "Градинарски услуги",
     description:
       "Проектиране, изграждане и поддръжка на зелени площи, абонаментна поддръжка на градини и дворове, затревяване с райграс и тревни чимове, засаждане на декоративни дървета, храсти, цветя и жив плет. Подрязване на жив плет, дървета и храсти. Озеленяване на градини, дворове, паркове и тераси. Косене на трева и райграс.",
-    icon: ShopOutlined,
+    icon: (
+      <DotLottieIcon src="https://assets2.lottiefiles.com/packages/lf20_ndCfQl/Flower.json" />
+    ),
     key: "gardening_services",
   },
   {
     title: "Борба с насекоми и вредители",
     description:
       "Услугата включва професионални методи за борба с насекоми и вредители.",
-    icon: ShopOutlined,
+    icon: (
+      <DotLottieIcon src="https://assets3.lottiefiles.com/packages/lf20_5uHqB6.json" />
+    ),
     key: "pest_control",
   },
 ];
@@ -91,11 +120,14 @@ const CleaningServicesCards = () => {
             key={service.key}
             hoverable
             onClick={() => showModal(service)}
+            style={{ padding: "10px" }}
           >
-            <div style={{ paddingBottom: "16px" }}>
-              <service.icon style={{ fontSize: "48px", color: "#1290cb" }} />
-            </div>
-            <h2>{service.title}</h2>
+            <Flex justify="center" align="center" vertical>
+              {service.icon}
+
+              <h2>{service.title}</h2>
+              <p style={{ color: "#1290cb" }}>Кликни за подробности</p>
+            </Flex>
           </Card.Grid>
         ))}
       </Card>
