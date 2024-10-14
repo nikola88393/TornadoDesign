692176fa3c53ff66d77833dea154e237e9184ec6
Nikola <nikola88393@gmail.com>
2024-10-14 13:53:49 +0300

    Refactor why choose service component to use ant design's built in flex component

--- src/components/WhyChooseServices.jsx
+++ src/components/WhyChooseServices.jsx
@@ -1,4 +1,4 @@
-import { Row, Col } from "antd";
+import { Row, Col, Flex } from "antd";
 import "../styles/WhyChooseServices.css";
 import arrow from "../assets/arrow-in-the-target.svg";
 const reasonsToChooseUs = [
@@ -30,29 +30,25 @@ const reasonsToChooseUs = [
 const WhyChooseServices = () => {
   return (
     <>
-      <div className="title-container">
+      <Flex align="center" vertical style={{ marginBottom: "30px" }}>
         <h1 className="reasons-to-choose-title">Защо да изберете нас?</h1>
         <img
           className="reasons-title-icon"
           src={arrow}
           alt="arrow in target icon"
         />
-      </div>
+      </Flex>
       <Row
         className="reasons-to-choose-container"
         gutter={[32, 32]}
         align="middle"
       >
         {reasonsToChooseUs.map((reason) => (
-          <Col
-            className="reason-container"
-            key={reason.number}
-            xs={24}
-            sm={12}
-            md={8}
-          >
-            <p className="reason-to-choose-number">{reason.number}</p>
-            <p className="reason-to-choose-text">{reason.text}</p>
+          <Col key={reason.number} xs={24} sm={12} md={8}>
+            <Flex gap={15} align="center">
+              <p className="reason-to-choose-number">{reason.number}</p>
+              <p className="reason-to-choose-text">{reason.text}</p>
+            </Flex>
           </Col>
         ))}
       </Row>
