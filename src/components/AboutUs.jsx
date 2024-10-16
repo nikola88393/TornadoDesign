da47bde025307d2dbf0bc631771a76e490866e09
Nikola <nikola88393@gmail.com>
2024-10-16 15:03:52 +0300

    WIP: Create customer review carousel component

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,54 +1,58 @@
 import { Row, Col } from "antd";
 import "../styles/AboutUs.css";
 import logo_big from "../assets/tornado_logo_big.png";
+import CustomerReviews from "./CustomerReviews";
 
 const AboutUs = () => {
   return (
-    <Row
-      gutter={[16, 32]}
-      align="middle"
-      justify="center"
-      wrap
-      style={{ padding: "20px", maxWidth: "1500px" }}
-    >
-      <Col
-        xs={24} // Full width on extra small screens (mobile)
-        sm={24} // Full width on small screens (mobile)
-        md={12} // Half width on medium screens (tablet)
-        lg={10} // Slightly less than half width on large screens (desktop)
+    <>
+      <Row
+        gutter={[16, 32]}
+        align="middle"
+        justify="center"
+        wrap
+        style={{ padding: "20px", maxWidth: "1500px" }}
       >
-        <div className="about-us-content">
-          <p style={{ fontSize: "17px", textAlign: "justify" }}>
-            Ние сме фирма започнала дейността си в сферата на почистването през
-            2016 г. в Лондон. През 2018 г. стъпихме на българския пазар под
-            името Торнадо Клийн.От 2020 г. Торнадо Сървисиз сключи 5 годишен
-            договор с най-голямата австралийска почистваща компания. В
-            партньорство с нея Торнадо започна да предлага услуги различни от
-            почистването, но пак свързани с нуждите на едно домакинство.
-            Успехите зад граница се прехвърлиха и в родината ни и смело се
-            утвърждава на пазара, вече не само в Англия, но и в България.
-          </p>
-        </div>
-      </Col>
+        <Col
+          xs={24} // Full width on extra small screens (mobile)
+          sm={24} // Full width on small screens (mobile)
+          md={12} // Half width on medium screens (tablet)
+          lg={10} // Slightly less than half width on large screens (desktop)
+        >
+          <div className="about-us-content">
+            <p style={{ fontSize: "17px", textAlign: "justify" }}>
+              Ние сме фирма започнала дейността си в сферата на почистването
+              през 2016 г. в Лондон. През 2018 г. стъпихме на българския пазар
+              под името Торнадо Клийн.От 2020 г. Торнадо Сървисиз сключи 5
+              годишен договор с най-голямата австралийска почистваща компания. В
+              партньорство с нея Торнадо започна да предлага услуги различни от
+              почистването, но пак свързани с нуждите на едно домакинство.
+              Успехите зад граница се прехвърлиха и в родината ни и смело се
+              утвърждава на пазара, вече не само в Англия, но и в България.
+            </p>
+          </div>
+        </Col>
 
-      <Col
-        xs={24} // Full width on extra small screens (mobile)
-        sm={24} // Full width on small screens (mobile)
-        md={12} // Half width on medium screens (tablet)
-        lg={8} // Slightly less than half width on large screens (desktop)
-        style={{ textAlign: "center" }} // Center the image on smaller screens
-      >
-        <img
-          style={{
-            width: "100%", // Let the image take full width of the column
-            maxWidth: "300px", // Limit max size to avoid stretching
-            margin: "0 auto", // Center the image on small screens
-          }}
-          src={logo_big}
-          alt="logo"
-        />
-      </Col>
-    </Row>
+        <Col
+          xs={24} // Full width on extra small screens (mobile)
+          sm={24} // Full width on small screens (mobile)
+          md={12} // Half width on medium screens (tablet)
+          lg={8} // Slightly less than half width on large screens (desktop)
+          style={{ textAlign: "center" }} // Center the image on smaller screens
+        >
+          <img
+            style={{
+              width: "100%", // Let the image take full width of the column
+              maxWidth: "300px", // Limit max size to avoid stretching
+              margin: "0 auto", // Center the image on small screens
+            }}
+            src={logo_big}
+            alt="logo"
+          />
+        </Col>
+      </Row>
+      <CustomerReviews />
+    </>
   );
 };
 
---
+++ src/components/CustomerReviews.jsx
@@ -0,0 +1,58 @@
+import { Carousel, Divider, Flex } from "antd";
+import "../styles/CustomerReviews.css";
+
+const Reviews = [
+  {
+    text: "Много приятен човек, страшно добре си свърши работата. Диванът блести, а аз тотално нямах ангажимент и можех да си върша работата, докато той си работеше.",
+    author: "Teodora B.",
+  },
+  {
+    text: "Благодаря на екипа! За ентусиазма и доброто отношение. Пропуснаха дребни детайли. Откровено казано и аз не ги виждах по време на действието. Препоръчвам горещо! Ако ми се наложи отново - дори ме бих търсила, директно ще искам същия екип. Браво за професионализма!",
+    author: "Екатерина г.",
+  },
+  {
+    text: "Постара се доста момчето да отстрани всички петна от децата по меката мебел. Повече впечатления ще имам след изсъхването на диваните. Благодаря!",
+    author: "Христина Й.",
+  },
+];
+
+const CustomerReviews = () => {
+  return (
+    <div style={{ maxWidth: "1000px", marginTop: "50px" }}>
+      <h1 style={{ color: "#1290cb", textAlign: "center" }}>
+        Клиентите за нас
+      </h1>
+      <p style={{ color: "#1290cb", textAlign: "center" }}>
+        Имаме изградена отлична репутация както в България така и във
+        Великобритания.
+      </p>
+      <p style={{ color: "#1290cb", textAlign: "center" }}>
+        Все повече клиенти ни се доверяват заради нашия професионализъм и
+        качество в извършените услуги.
+      </p>
+      <Carousel
+        infinite
+        style={{ width: "100%", margin: "0 auto" }} // Fully responsive width
+        autoplay
+        arrows
+        autoplaySpeed={5000}
+      >
+        {Reviews.map((review) => (
+          <Flex
+            style={{ border: "1px solid red", padding: "20px" }}
+            key={review.author}
+            align="center"
+            justify="center"
+          >
+            <Divider />
+            <p style={{ textAlign: "center" }}>&quot;{review.text}&quot;</p>
+            <p style={{ textAlign: "center" }}> - {review.author}</p>
+            <Divider />
+          </Flex>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
+
+export default CustomerReviews;
